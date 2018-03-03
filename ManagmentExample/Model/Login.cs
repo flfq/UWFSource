1b81a037bc029a49f5cc60d5fd63d45a91b25e17
Khoi Minh Hoang <khoihn1@fsoft.com.vn>
2018-03-03 18:45:29 +0700

    upload project

---
+++ ManagmentExample/Model/Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagmentExample.Model
+{
+    public class Login
+    {
+        public Login()
+        {
+
+        }
+
+        public string UserName;
+        public string Password;
+
+    }
+}
